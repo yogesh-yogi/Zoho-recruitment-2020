93a897eb413f81d0761179ad4a0b801cf720b6cb
Yogesh Raj <72138395+yogesh-yogi@users.noreply.github.com>
2021-02-01 10:18:13 +0530

    Add files via upload

--- Demo.c
+++ Demo.c
@@ -1,1 +1,20 @@
-
+#include <stdio.h>
+ 
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int i,j,k;
+	i=n;j=n;
+	static int z;
+	for(k=0;k<n;k++)
+	{
+		while(i-->0)
+		printf(" ");
+		while(j>0)
+		{
+			printf("%d",++z);
+			}
+			i=n-1;
+}}
+ 
